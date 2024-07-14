5bd7dccc4183e36b5c9fe3d4a069b4104132729d
ZgjimDida <zgjimidida@gmail.com>
2024-07-14 15:00:50 +0200

    Set sorted books by most experts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,12 @@ export class AppComponent {
   allBooks = allBooks;
   bookTitles: any = [];
   recommenders: any = [];
+  sortedBooks: any = [];
 
   ngOnInit() {
     this.setBooks();
     this.setRecommenders();
+    this.setSortedBooks(this.allBooks);
   }
 
   setBooks() {
@@ -23,6 +25,17 @@ export class AppComponent {
 
   setRecommenders() {
     this.recommenders = [... new Set(this.allBooks.map(item => item.recommender))];
+  }
+
+  setSortedBooks(booksToSort: any[]) {
+    this.sortedBooks = [];
+    let recommenders = [];
+    for (let i = 0; i < this.bookTitles.length; i++) {
+      booksToSort.filter(item => item.title === this.bookTitles[i]);
+      recommenders = booksToSort.filter(item => item.title === this.bookTitles[i]).map(item => item.recommender);
+      this.sortedBooks.push({ title: this.bookTitles[i], recommenders: recommenders })
+    }
+    this.sortedBooks.sort((a: any, b: any) => { return b.recommenders.length - a.recommenders.length })
   }
 
 }
