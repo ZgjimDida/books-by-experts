db1677e11776b07ba803aed655ee5357e93a3598
ZgjimDida <zgjimidida@gmail.com>
2024-07-14 15:08:50 +0200

    Select books by expert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { allBooks } from './variables';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
   allBooks = allBooks;
   bookTitles: any = [];
   recommenders: any = [];
   sortedBooks: any = [];
+  selectedAuthors: any[] = [];
+  selectedBooks: any[] = [];
 
   ngOnInit() {
     this.setBooks();
@@ -36,6 +37,32 @@ export class AppComponent {
       this.sortedBooks.push({ title: this.bookTitles[i], recommenders: recommenders })
     }
     this.sortedBooks.sort((a: any, b: any) => { return b.recommenders.length - a.recommenders.length })
+    this.sortedBooks = this.sortedBooks.filter((item: any) => item.recommenders.length > 0)
   }
 
+  isSelected(recommender: string) {
+    let isSelected = false;
+    if (this.selectedAuthors.includes(recommender)) {
+      isSelected = true;
+    }
+    return isSelected;
+  }
+
+  select(recommender: string) {
+    if (!this.selectedAuthors.includes(recommender)) {
+      this.selectedAuthors.push(recommender);
+    }
+    this.setSelectedBooks();
+  }
+
+  setSelectedBooks() {
+    let selectedBooks = [];
+    for (let i = 0; i < this.allBooks.length; i++) {
+      if (this.selectedAuthors.includes(this.allBooks[i].recommender)) {
+        selectedBooks.push(this.allBooks[i]);
+      }
+    }
+    this.selectedBooks = selectedBooks;
+    this.setSortedBooks(this.selectedBooks);
+  }
 }
