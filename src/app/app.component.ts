c690fa7c58f4a4e09b1681f419f0ae28a81842f5
ZgjimDida <zgjimidida@gmail.com>
2024-07-14 20:23:16 +0200

    Refactor home component and helper service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,6 @@ import { HelperService } from './helper.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  allBooks = allBooks;
-  books: any = [];
-  experts: any = [];
-  sortedBooks: any = [];
   selectedAuthors: any[] = [];
   selectedBooks: any[] = [];
 
@@ -20,76 +16,33 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    this.setBooks();
-    this.setExperts();
-    this.setSortedBooks(this.allBooks);
-    this.removeDuplicates();
+    this.helper.setup();
   }
 
-  setBooks() {
-    this.books = [... new Set(this.allBooks.map(item => item.title))];
-    this.helper.setBooks(this.books);
-  }
+  // isSelected(recommender: string) {
+  //   let isSelected = false;
+  //   if (this.selectedAuthors.includes(recommender)) {
+  //     isSelected = true;
+  //   }
+  //   return isSelected;
+  // }
 
-  setExperts() {
-    this.experts = [... new Set(this.allBooks.map(item => item.recommender))];
-    this.experts.sort();
-    this.helper.setExperts(this.experts);
-  }
+  // select(recommender: string) {
+  //   if (!this.selectedAuthors.includes(recommender)) {
+  //     this.selectedAuthors.push(recommender);
+  //   }
+  //   this.setSelectedBooks();
+  // }
 
-  setSortedBooks(booksToSort: any[]) {
-    this.sortedBooks = [];
-    let experts = [];
-    for (let i = 0; i < this.books.length; i++) {
-      booksToSort.filter(item => item.title === this.books[i]);
-      experts = booksToSort.filter(item => item.title === this.books[i]).map(item => item.recommender);
-      this.sortedBooks.push({ title: this.books[i], experts: experts })
-    }
-    this.sortedBooks.sort((a: any, b: any) => { return b.experts.length - a.experts.length })
-    this.sortedBooks = this.sortedBooks.filter((item: any) => item.experts.length > 0)
-  }
+  // setSelectedBooks() {
+  //   let selectedBooks = [];
+  //   for (let i = 0; i < this.allBooks.length; i++) {
+  //     if (this.selectedAuthors.includes(this.allBooks[i].recommender)) {
+  //       selectedBooks.push(this.allBooks[i]);
+  //     }
+  //   }
+  //   this.selectedBooks = selectedBooks;
+  //   this.setSortedBooks(this.selectedBooks);
+  // }
 
-  isSelected(recommender: string) {
-    let isSelected = false;
-    if (this.selectedAuthors.includes(recommender)) {
-      isSelected = true;
-    }
-    return isSelected;
-  }
-
-  select(recommender: string) {
-    if (!this.selectedAuthors.includes(recommender)) {
-      this.selectedAuthors.push(recommender);
-    }
-    this.setSelectedBooks();
-  }
-
-  setSelectedBooks() {
-    let selectedBooks = [];
-    for (let i = 0; i < this.allBooks.length; i++) {
-      if (this.selectedAuthors.includes(this.allBooks[i].recommender)) {
-        selectedBooks.push(this.allBooks[i]);
-      }
-    }
-    this.selectedBooks = selectedBooks;
-    this.setSortedBooks(this.selectedBooks);
-  }
-
-  convertName(name: string) {
-    return this.helper.convertName(name);
-  }
-
-  removeDuplicates() {
-    console.log(this.allBooks);
-    let newArray = [];
-    for (let i = 0; i < this.allBooks.length; i++) {
-      newArray.push(this.allBooks[i].recommender + "THESPLIT" + this.allBooks[i].title);
-    }
-    newArray = [... new Set(newArray)];
-    let newArray2 = [];
-    for (let i = 0; i < newArray.length; i++) {
-      newArray2.push({ "title": newArray[i].split("THESPLIT")[1], "recommender": newArray[i].split("THESPLIT")[0] });
-    }
-    console.log(newArray2);
-  }
 }
--- src/app/helper.service.ts
+++ src/app/helper.service.ts
@@ -1,16 +1,46 @@
 import { Injectable } from '@angular/core';
+import { allBooks } from './variables';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
+  allBooks = allBooks;
   books: any[] = [];
   experts: any[] = [];
+  sortedBooks: any = [];
 
   constructor() { }
 
   test() {
     console.log("test");
+  }
+
+  setup() {
+    this.setupBooks();
+    this.setupExperts();
+    this.setupSortedBooks(this.allBooks);
+  }
+
+  setupBooks() {
+    this.books = [... new Set(this.allBooks.map(item => item.title))];
+  }
+
+  setupExperts() {
+    this.experts = [... new Set(this.allBooks.map(item => item.recommender))];
+    this.experts.sort();
+  }
+
+  setupSortedBooks(booksToSort: any[]) {
+    this.sortedBooks = [];
+    let experts = [];
+    for (let i = 0; i < this.books.length; i++) {
+      booksToSort.filter(item => item.title === this.books[i]);
+      experts = booksToSort.filter(item => item.title === this.books[i]).map(item => item.recommender);
+      this.sortedBooks.push({ title: this.books[i], experts: experts })
+    }
+    this.sortedBooks.sort((a: any, b: any) => { return b.experts.length - a.experts.length })
+    this.sortedBooks = this.sortedBooks.filter((item: any) => item.experts.length > 0)
   }
 
   getBooks() {
@@ -21,15 +51,21 @@ export class HelperService {
     return this.experts;
   }
 
-  setBooks(books: any[]){
-    this.books = books;
-  }
-
-  setExperts(experts: any[]) {
-    this.experts = experts;
-  }
-
   convertName(name: string) {
     return name.replaceAll('-', ' ');
+  }
+
+  removeDuplicates() {
+    console.log(this.allBooks);
+    let newArray = [];
+    for (let i = 0; i < this.allBooks.length; i++) {
+      newArray.push(this.allBooks[i].recommender + "THESPLIT" + this.allBooks[i].title);
+    }
+    newArray = [... new Set(newArray)];
+    let newArray2 = [];
+    for (let i = 0; i < newArray.length; i++) {
+      newArray2.push({ "title": newArray[i].split("THESPLIT")[1], "recommender": newArray[i].split("THESPLIT")[0] });
+    }
+    console.log(newArray2);
   }
 }
